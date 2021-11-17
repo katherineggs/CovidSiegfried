ae7f025dd6b7acfb34c77826433e3f4c2269e61d
katherineggs <katherinegarcia@ufm.edu>
2021-11-17 15:01:00 -0600

    Dags and funcs corrected

--- dags/tryDags.py
+++ dags/tryDags.py
@@ -29,44 +29,60 @@ def processFile(**kwargs):
     conf = dfConfirmed.melt(id_vars=["Province/State", "Country/Region", "Lat", "Long"],
                             var_name="Date",
                             value_name="Confirmed")
-    #to date type
+    # to date type
     conf['Date'] = pd.to_datetime(conf['Date'])
 
-    #add new columns
+    # add new columns
     conf['Year'] = conf['Date'].dt.year
     conf['Month'] = conf['Date'].dt.month
     conf['Day'] = conf['Date'].dt.day
 
+    # Meter a la db
+    source = MySqlHook('mydb').get_sqlalchemy_engine()
+
+    with source.begin() as connection:
+        conf.to_sql('CovidConfirmed', schema='test', con=connection, if_exists='append', chunksize=2500, index=False)
+
+
+def processDeaths(**kwargs):
     # # Deaths
     # --- read data
-    filepath = f"{FSHook('fs_default').get_path()}/time_series_covid19_deaths_global.csv"
+    filepath = f"{FSHook('fs_default1').get_path()}/time_series_covid19_deaths_global.csv"
     dfDeaths = pd.read_csv(filepath)
 
     # --- order data
     deaths = dfDeaths.melt(id_vars=["Province/State", "Country/Region", "Lat", "Long"],
-                            var_name="Date",
-                            value_name="Deaths")
-    #to date type
+                           var_name="Date",
+                           value_name="Deaths")
+    # to date type
     deaths['Date'] = pd.to_datetime(deaths['Date'])
 
-    #add new columns
+    # add new columns
     deaths['Year'] = deaths['Date'].dt.year
     deaths['Month'] = deaths['Date'].dt.month
     deaths['Day'] = deaths['Date'].dt.day
 
+    # Meter a la db
+    source = MySqlHook('mydb').get_sqlalchemy_engine()
+
+    with source.begin() as connection:
+        deaths.to_sql('CovidDeaths', schema='test', con=connection, if_exists='append', chunksize=2500, index=False)
+
+
+def processRecovered(**kwargs):
     # # Recovered
     # --- read data
-    filepath = f"{FSHook('fs_default').get_path()}/time_series_covid19_recovered_global.csv"
+    filepath = f"{FSHook('fs_default2').get_path()}/time_series_covid19_recovered_global.csv"
     dfRecovered = pd.read_csv(filepath)
 
     # --- order data
     recovered = dfRecovered.melt(id_vars=["Province/State", "Country/Region", "Lat", "Long"],
-                            var_name="Date",
-                            value_name="Recovered")
-    #to date type
+                                 var_name="Date",
+                                 value_name="Recovered")
+    # to date type
     recovered['Date'] = pd.to_datetime(recovered['Date'])
 
-    #add new columns
+    # add new columns
     recovered['Year'] = recovered['Date'].dt.year
     recovered['Month'] = recovered['Date'].dt.month
     recovered['Day'] = recovered['Date'].dt.day
@@ -75,9 +91,9 @@ def processFile(**kwargs):
     source = MySqlHook('mydb').get_sqlalchemy_engine()
 
     with source.begin() as connection:
-        conf.to_sql('CovidSiegfried', schema='test', con=connection, if_exists='append', chunksize=2500, index=False)
-        deaths.to_sql('CovidSiegfried', schema='test', con=connection, if_exists='append', chunksize=2500, index=False)
-        recovered.to_sql('CovidSiegfried', schema='test', con=connection, if_exists='append', chunksize=2500, index=False)
+        recovered.to_sql('CovidRecovered', schema='test',
+                         con=connection, if_exists='append',
+                         chunksize=2500, index=False)
 
 
 t1 = PythonOperator(
@@ -90,20 +106,47 @@ t1 = PythonOperator(
 )
 
 
-sensorTask = FileSensor(task_id="checkFile",
+sensorConf = FileSensor(task_id="checkConf",
                         dag=dag,
                         poke_interval=10,
                         fs_conn_id="fs_default",
                         filepath="time_series_covid19_confirmed_global.csv",
                         timeout=60)
 
-processFileOperator = PythonOperator(
-    task_id='procesarArchivo',
+sensorDeath = FileSensor(task_id="checkDeaths",
+                         dag=dag,
+                         poke_interval=10,
+                         fs_conn_id="fs_default1",
+                         filepath="time_series_covid19_deaths_global.csv",
+                         timeout=60)
+
+sensorRecov = FileSensor(task_id="checkRecovered",
+                         dag=dag,
+                         poke_interval=10,
+                         fs_conn_id="fs_default2",
+                         filepath="time_series_covid19_recovered_global.csv",
+                         timeout=60)
+
+processFileConf = PythonOperator(
+    task_id='procesarConf',
     dag=dag,
     python_callable=processFile,
     provide_context=True,
-    op_kwargs={
-    }
+    op_kwargs={}
+)
+processFileDeath = PythonOperator(
+    task_id='procesarDeath',
+    dag=dag,
+    python_callable=processDeaths,
+    provide_context=True,
+    op_kwargs={}
+)
+processFileRec = PythonOperator(
+    task_id='procesarRec',
+    dag=dag,
+    python_callable=processRecovered,
+    provide_context=True,
+    op_kwargs={}
 )
 
-sensorTask >> processFileOperator
+sensorConf >> processFileConf >> sensorDeath >> processFileDeath >> sensorRecov >> processFileRec
